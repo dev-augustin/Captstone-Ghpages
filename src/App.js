576d5a26db390f262de6813fea8e1e31f94594b9
dev-augustin <dev.augustin00@gmail.com>
2020-06-03 17:15:16 -0500

    added Hash router

--- src/App.js
+++ src/App.js
@@ -24,14 +24,14 @@ function App() {
        <div>
          <Navigation/>
          <Switch>
-            <Route exact path = '/' component={Home}></Route>
-            <Route exact path = '/myCart' component={MyCart}></Route>
-            <Route exact path = '/myProfile' component={MyProfile}></Route>
-            <Route exact path = '/giftBasket/3.1' component={GitftBasket}></Route>
-            <Route exact path = '/flowers/3.2' component={Flowers}></Route>
+            <Route exact path = 'Captstone-Ghpages/' component={Home}></Route>
+            <Route exact path = 'Captstone-Ghpages/myCart' component={MyCart}></Route>
+            <Route exact path = 'Captstone-Ghpages/myProfile' component={MyProfile}></Route>
+            <Route exact path = 'Captstone-Ghpages/giftBasket/3.1' component={GitftBasket}></Route>
+            <Route exact path = '/Captstone-Ghpagesflowers/3.2' component={Flowers}></Route>
             {/* <Route exact path = '/handmade/3.1' component={Handmade}></Route> */}
             <Route exact path = '/review' component={Review}></Route>
-            <Route exact path = '/contact' component={Contact}></Route>
+            <Route exact path = 'Captstone-Ghpages/contact' component={Contact}></Route>
             <Route exact path = '/view' component={View}></Route>
             <Route exact path = '/editorder/:id' component={EditOrder}></Route>
             <Route exact path = '/confirmation' component={Confirmation}></Route>
