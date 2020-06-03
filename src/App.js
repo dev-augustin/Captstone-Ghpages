1e1dc41850130e3fde45e415e98cfe06615ccedc
dev-augustin <dev.augustin00@gmail.com>
2020-06-03 17:19:47 -0500

    added Hash router

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 // import './App.css';
-import {BroswerRouter as Router, Switch, Route} from 'react-router-dom'
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom'
 import './App.scss'
 // import Search from './components/Search'
 import MyCart from './components/MyCart'
