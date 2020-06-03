166a5a9d5edd037809fe982b3bb604511a7b5409
dev-augustin <dev.augustin00@gmail.com>
2020-06-03 16:45:43 -0500

    Updates

---
+++ precache-manifest.48dcf7af58507f3e57fc0fb72a56b382.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8cb8f13657522838b282e6fb99564be1",
+    "url": "/dev-augustin.github.io/index.html"
+  },
+  {
+    "revision": "8d26d168b02ce925fed5",
+    "url": "/dev-augustin.github.io/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "c7d36b82a6e3611668cc",
+    "url": "/dev-augustin.github.io/static/css/main.048a49c6.chunk.css"
+  },
+  {
+    "revision": "8d26d168b02ce925fed5",
+    "url": "/dev-augustin.github.io/static/js/2.a31a58df.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/dev-augustin.github.io/static/js/2.a31a58df.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c7d36b82a6e3611668cc",
+    "url": "/dev-augustin.github.io/static/js/main.91e1d10e.chunk.js"
+  },
+  {
+    "revision": "76d57e3bafa6ec877523",
+    "url": "/dev-augustin.github.io/static/js/runtime-main.be7ec8c2.js"
+  },
+  {
+    "revision": "52d5edd1fb04024a029a75dcc5942ebc",
+    "url": "/dev-augustin.github.io/static/media/birthday-table.52d5edd1.jpeg"
+  },
+  {
+    "revision": "d4b56f62a4ee41d247ad2f3d95f7a97e",
+    "url": "/dev-augustin.github.io/static/media/biscuits.d4b56f62.jpeg"
+  },
+  {
+    "revision": "d8b9962980663ab0ab61c80d2708ab56",
+    "url": "/dev-augustin.github.io/static/media/flowers.d8b99629.jpeg"
+  }
+]);
